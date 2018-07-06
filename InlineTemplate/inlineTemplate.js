11e34d1af2d2a487b3a2ff3e101fb5b58e9a82d9
yongdu <yongdu90@gmail.com>
2018-07-06 22:33:03 +0200

    vue loader

---
+++ InlineTemplate/inlineTemplate.js
@@ -0,0 +1,8 @@
+Vue.component('progress-view', {
+    data(){
+        return {completionRate: 20}
+    }
+})
+new Vue({
+    el: '#root',
+});
---
+++ my-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <message>Hello There!</message>
+    <message>Hello Here!</message>
+    <message>Hello Alla!</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+export default {
+  name: 'app',
+  components:{Message},
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
